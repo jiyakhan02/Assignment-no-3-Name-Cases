8fda355264ca4a0b3ecca5e88c61ac226a87ace5
JaweriyaMursleen <160059863+jiyakhan02@users.noreply.github.com>
2024-03-24 04:32:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-"use strict";
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 let person_Name = "Abaan";
 // in lowercase
